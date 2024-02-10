f4ea94424f69f8b7ec1be97bfaf0f008030166e4
r3s3aechAnDd3v3lopm3nt <159447400+r3s3aechAnDd3v3lopm3nt@users.noreply.github.com>
2024-02-10 21:34:16 +0700

    upload javascript example for comment code

---
+++ script.js
@@ -0,0 +1,15 @@
+// Creating a variable using var keyword
+// var VariableName = 'Test';
+
+// Creating a variable using let keyword
+// let VariableName = 'Test';
+
+// Creating a variable using const keyword
+// const VariableName = 'Test';
+
+//Variable Naming
+//1. The name should be unique
+//2. The name should not be unique be anyword reversed keyword
+//3. The name must start with a character, _, &.
+
+// console.log(VariableName);
