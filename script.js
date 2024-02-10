e5b89bc001f22fd5d2cc4e35f5b1f8cf36baeb4b
r3s3aechAnDd3v3lopm3nt <159447400+r3s3aechAnDd3v3lopm3nt@users.noreply.github.com>
2024-02-10 23:29:47 +0700

    04_if_statement_exmaple

---
+++ script.js
@@ -0,0 +1,15 @@
+// Lesson Logic and Control Flow
+
+const age = 18;
+
+// if (condidtion) {
+    // block of code
+// }
+
+if (age > 18) {
+    console.log('You may enter!');
+} else if (age === 18) {
+    console.log('You just turned 18, welcome');
+} else {
+    console.log('Grow up!');
+}
